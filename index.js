eef172fee92219bee27c715f09a44d20587a8144
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-05-06 10:04:43 -0500

    saved minor changes

--- index.js
+++ index.js
@@ -53,22 +53,6 @@ server.get('/api/users/:id', (req, res) => {
         res.status(500).json({ errorMessage: "The user information could not be retrieved." })
     }
 
-
-    // if(id != users.id) {
-        
-    //     res.status(404).json({ errorMessage: "The User with the specified ID does not exist." }); 
-
-    // }
-
-    // else if (!users) {
-
-    //     res.status(500).json({ errorMessage: "The user information could not be retrieved." }); 
-
-    // }
-
-    // else {
-    //     res.status(200).json(users); 
-    // }
 })
 
 server.post('/api/users', (req, res) => {
